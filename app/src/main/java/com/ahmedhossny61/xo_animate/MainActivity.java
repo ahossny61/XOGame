055c6f6b36fc38222c262629231e72fc4da0627c
ahossny61 <ahossny61@gmail.com>
2022-01-08 14:06:59 +0200

    init commit

---
+++ app/src/main/java/com/ahmedhossny61/xo_animate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ahmedhossny61.xo_animate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
